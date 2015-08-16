9c27dbab77a632a8d58e8a821b3142b3c05609f6
Eric Martz <ericmartz@gmail.com>
2015-08-16 15:37:29 -0400

    Added player class, and functions for render, handleInput and update.

--- js/app.js
+++ js/app.js
@@ -24,12 +24,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+}
 
+Player.prototype.render = function() {
+  //Test
+};
+
+Player.prototype.handleInput = function() {
+  //Test
+}
+
+Player.prototype.update = function() {
+  //Test
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var player = new Player;
+var allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
