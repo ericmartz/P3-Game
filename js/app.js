148ad10dd478c5d9ec6fd6972650ce877a6e024e
Eric Martz <ericmartz@gmail.com>
2015-08-20 16:27:05 -0400

    Added enemies to game.

--- js/app.js
+++ js/app.js
@@ -1,41 +1,57 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  console.log(dt);
+  this.x += this.speed * dt;
+  this.checkBounds();
 }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Enemy.prototype.checkBounds = function() {
+  if (this.x >= 515){
+    this.x = -100;
+  }
 }
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-  // this = Object.create(Player.prototype)
   this.sprite = 'images/char-boy.png';
   //TODO: I know the canvas width is 505 and height is 606.
   //TODO: I feel these initial positions should be calculated dynamically, but using ctx.canvas.width/height
   //TODO: does not seem to work.  I can reason out that it is out of scope from this function, but in player.render()
   //TODO: it is in scope.  Trying to figure this out, but spent about 10 hours on it and I need to move on for the momoment
   //TODO: so this is becoming a TODO.
-  this.positionX = (505 / 2) - 50;
-  this.positionY = 606 - 200;
+  this.positionX = (505 / 2) - 50; //Initial X position
+  this.positionY = 606 - 200; //Initial Y position
   this.moveX = 0;
   this.moveY = 0;
+}
+
+Player.prototype.playerSafe = function() {
+  this.positionX = (505 / 2) - 50;
+  this.positionY = 606 - 200;
 }
 
 Player.prototype.render = function() {
@@ -48,7 +64,7 @@ Player.prototype.render = function() {
 // handleInput() takes in the keyCode, determines which key was pressed and sets the number of pixels the player will move.
 Player.prototype.handleInput = function(keyCode) {
   if (keyCode === 'up'){
-    if (player.positionY === -9) {
+    if (this.positionY === -9) {
       this.moveX = 0;
       this.moveY = 0;
     } else {
@@ -56,7 +72,7 @@ Player.prototype.handleInput = function(keyCode) {
       this.moveY = -83;
     }
   } else if (keyCode === 'down'){
-    if (player.positionY === 406) {
+    if (this.positionY === 406) {
       this.moveX = 0;
       this.moveY = 0;
     } else {
@@ -64,7 +80,7 @@ Player.prototype.handleInput = function(keyCode) {
       this.moveY = 83;
     }
   } else if (keyCode === 'left'){
-    if (player.positionX === 2.5) {
+    if (this.positionX === 2.5) {
       this.moveX = 0;
       this.moveY = 0;
     } else {
@@ -72,7 +88,7 @@ Player.prototype.handleInput = function(keyCode) {
       this.moveY = 0;
     }
   } else if (keyCode === 'right'){
-    if (player.positionX === 402.5) {
+    if (this.positionX === 402.5) {
       this.moveX = 0;
       this.moveY = 0;
     } else {
@@ -88,6 +104,10 @@ Player.prototype.update = function() {
   this.positionX += this.moveX;
   this.positionY += this.moveY;
 
+  if (this.positionY === -9) {
+    this.playerSafe();
+  }
+
   this.moveX = 0;
   this.moveY = 0;
 }
@@ -95,7 +115,11 @@ Player.prototype.update = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
+var allEnemies = [
+  //TODO: Create way to randamnly generate enemies?  
+  enemy1 = new Enemy(1, 230, 100),
+  enemy1 = new Enemy(-150, 147, 200)
+];
 var player = new Player();
 
 
