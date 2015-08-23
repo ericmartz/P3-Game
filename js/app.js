463070075767e2241dfbbce20d6084f7186b8ca9
Eric Martz <ericmartz@gmail.com>
2015-08-23 14:48:09 -0400

    Deleted unnecessary comments from app.js

--- js/app.js
+++ js/app.js
@@ -1,29 +1,18 @@
-// Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.width = 75; //width of the bug sprite is actually 101, but making it 75 is close to where the characters actually touch.
+var Enemy = function(x, y, speed) {
+  this.sprite = 'images/enemy-bug.png';
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.width = 75; //width of the bug sprite is actually 101, but making it 75 is close to where the characters actually touch.
 }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
   this.x += this.speed * dt;
   this.checkBounds();
   this.checkCollisions();
 }
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
@@ -42,9 +31,6 @@ Enemy.prototype.checkCollisions = function() {
   }
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 var Player = function() {
   this.sprite = 'images/char-boy.png';
   //TODO: I know the canvas width is 505 and height is 606.
@@ -122,9 +108,6 @@ Player.prototype.update = function() {
   this.moveY = 0;
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 var allEnemies = [
   //TODO: Create way to randamnly generate enemies?
   // Enemy constuctor takes 3 arguments, x. y. and speed. 
@@ -139,9 +122,6 @@ var allEnemies = [
 ];
 var player = new Player();
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
